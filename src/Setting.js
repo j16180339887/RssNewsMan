8ae16fd79719102a53d676d0889df4bb742c4b04
Joeky Zhan <jj16180339887@gmail.com>
2019-01-17 12:29:23 +0800

    Cache error
    Signed-off-by: Joeky Zhan <jj16180339887@gmail.com>

--- src/Setting.js
+++ src/Setting.js
@@ -32,17 +32,6 @@ export default class Setting extends React.Component {
     }
 
     addNewFeed = () => {
-        // fetch('https://feeds.feedburner.com/techbang', {method: 'GET'})
-        // .then((response) => {
-        // console.log(JSON.stringify(response, null, 4))
-        // console.log(JSON.stringify(response.text(), null, 4))
-        // const parsed = new DomParser().parseFromString(response.text(), 'text/xml');
-        // console.log(parsed)
-        // parseString(response.text(), {trim: true}, (err, res) => {console.log(err, res)})
-        // })
-        // .catch((error) => {
-        // console.error(error);
-        // });
 
         let headers = new Headers({
             "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:59.0) Gecko/20100101 Firefox/59.0 (Chrome)",
@@ -56,36 +45,39 @@ export default class Setting extends React.Component {
         //         cache: 'default',
         //         keepalive: true
         //     })
-        //     .then((response) => {
-        //         // console.log(response)
-        //         // console.log("--------------------")
-        //         // console.log("--------------------")
-        //         // // console.log(response.body())
-        //         // console.log(response.text())
-        //         // const parser = new DOMParser();
-        //         // const parsed = DOMParser.parseFromString(response, 'text/html');
-        //         // console.log(JSON.stringify(parsed, null, 4))
-        //         // console.log(parsed.querySelect("meta[property='og:image']").getAttribute("content"))
-        //     })
 
-        var feeds = ["https://feeds.feedburner.com/techbang", "https://technews.tw/tn-rss"]
+        var feeds = [
+            "https://feeds.feedburner.com/techbang",
+            "https://technews.tw/tn-rss",
+            "https://www.gamebase.com.tw/news/rss/0",
+            "http://news.everydayhealth.com.tw/feed",
+            // "http://feeds.bbci.co.uk/zhongwen/trad/rss.xml"
+        ]
 
         Promise
         .all(feeds.map(this.getArticles))
+        .then((rssitem) => {
+            console.log("All done!")
+            // console.log(rssitem)
+        })
+        .catch((err) => {
+            console.log(err)
+        })
     }
 
     getArticles = (url) =>
         fetch(url)
         .then((response) => response.text())
         .then((responseData) => rssParser.parse(responseData))
-        .then((rss) => {
-            return Promise
+        .then((rss) =>
+            Promise
             .all(rss.items.map(this.getImageUrl))
-            .then((imgurl) => {
-                console.log("imgurl:")
-                console.log(imgurl)
-                return imgurl
+            .then((rssitem) => {
+                return rssitem
             })
+        )
+        .catch((err) => {
+            console.log(err)
         })
 
 
@@ -100,80 +92,15 @@ export default class Setting extends React.Component {
                 rssitem["image"] = /(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/i.exec(meta[0])[0]
                 // console.log(rssitem["links"][0]["url"])
                 // console.log(rssitem["image"])
-                // console.log(meta)
-                return (rssitem["image"])
+                // console.log(rssitem)
+                return rssitem
             }
-
             console.log("====================END=======================")
-            return ("FFFFaillll!")
+            return {}
         })
-
-
-    // getImageUrl = (url) => {
-    //     console.log(url)
-    //     let headers = {
-    //         "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:59.0) Gecko/20100101 Firefox/59.0 (Chrome)",
-    //         "Accept": "*/*",
-    //     };
-    //     var xhr = new XMLHttpRequest();
-    //     xhr.open("GET", url);
-    //     Object.keys(headers).map(function (key, index) {
-    //         xhr.setRequestHeader(key, headers[key]);
-    //     });
-    //     // xhr.setRequestHeader("Accept", headers["Accept"]);
-    //     // xhr.setRequestHeader("Content-Type", headers["Content-Type"]);
-    //     // xhr.setRequestHeader("Accept-Encoding", headers["accept-encoding"]);
-    //     xhr.timeout = 2000;
-    //     xhr.onload = () => {
-    //         // console.log(xhr.readyState == XMLHttpRequest.DONE)
-    //         console.log(xhr.readyState)
-    //         switch (xhr.status) {
-    //             case 200:
-    //                 var html = xhr._response
-    //                 var m;
-
-    //                 do {
-    //                     m = /<meta.*>/.exec(html)
-    //                     if (m && /og:image/.test(m[0])) {
-    //                         console.log(/(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/i.exec(m[0])[0])
-    //                         // var prop_re = /<meta.*>/g
-    //                     }
-    //                 } while (m);
-    //                 console.log("------------------")
-    //                 console.log("------------------")
-    //                 var doc = new DOMParser().parseFromString(
-    //                     '<xml xmlns="a" xmlns:c="./lite">\n' +
-    //                     '\t<child>test</child>\n' +
-    //                     '\t<child></child>\n' +
-    //                     '\t<child/>\n' +
-    //                     '</xml>', 'text/xml');
-    //                 doc.documentElement.setAttribute('x', 'y');
-    //                 doc.documentElement.setAttributeNS('./lite', 'c:x', 'y2');
-    //                 var nsAttr = doc.documentElement.getAttributeNS('./lite', 'x')
-    //                 // var doc = new DOMParser().parseFromString(xhr._response, "text/html");
-    //                 // console.log(new DOMParser().parseFromString(xhr._response, "text/html"))
-    //                 break;
-
-    //             default:
-    //                 console.log(xhr.status)
-    //                 break;
-    //         }
-    //     }
-    //     xhr.onreadystatechange = function () { //Call a function when the state changes.
-    //         console.log("ready", xhr.readyState, XMLHttpRequest.DONE)
-    //         if (xhr.readyState == XMLHttpRequest.DONE && xhr.status == 200) {
-    //             console.log("ready2")
-    //             console.log(xhr)
-    //             // Request finished. Do processing here.
-    //         }
-    //     }
-    //     xhr.ontimeout = function (e) {
-    //         console.log("timeout", e)
-    //     };
-    //     // console.log(xhr)
-
-    //     xhr.send();
-    // }
+        .catch((err) => {
+            console.log(err)
+        })
 
     render() {
         return (
